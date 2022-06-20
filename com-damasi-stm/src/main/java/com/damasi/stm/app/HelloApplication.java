14da731822dae65c85a1f3735fa1f5253b7b134a
DAMASI-Bauer <baueer19@gmail.com>
2022-06-20 18:45:17 +0200

    Projekt Struktur ändern

--- com-damasi-stm/src/main/java/com/damasi/stm/app/HelloApplication.java
+++ com-damasi-stm/src/main/java/com/damasi/stm/app/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.comdamasistm;
+package com.damasi.stm.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        stage.setTitle("StoryTeller");
         stage.setScene(scene);
         stage.show();
     }
